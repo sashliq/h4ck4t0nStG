be325694f8132951762f06548e8a8f89115f771c
my5t3ry <sascha.bast@dgm-kommunikation.de>
2016-11-04 22:34:45 +0100

    init

---
+++ carshare_backend/src/main/java/de/h4ck4t0n/CarshareApplication.java
@@ -0,0 +1,12 @@
+package de.h4ck4t0n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarshareApplication.class, args);
+	}
+}
