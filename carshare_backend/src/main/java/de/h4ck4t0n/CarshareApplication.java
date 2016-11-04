c22ea9ba4a663c230c035e0f0bbb83393ab0c00e
my5t3ry <sascha.bast@dgm-kommunikation.de>
2016-11-04 22:58:08 +0100

    init

--- carshare_backend/src/main/java/de/h4ck4t0n/CarshareApplication.java
+++ carshare_backend/src/main/java/de/h4ck4t0n/CarshareApplication.java
@@ -1,11 +1,12 @@
 package de.h4ck4t0n;
 
+import org.jsondoc.spring.boot.starter.EnableJSONDoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJSONDoc
 public class CarshareApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CarshareApplication.class, args);
 	}
